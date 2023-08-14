281de00788b67c8bee71215d305ec23b80ac9751
Do6ryjKot <arkhanghel@mail.ru>
2023-08-14 17:07:04 +0300

    Model repositories and manager configured.

---
+++ HeadHunterAnalyzer.API/Contracts/DataServices/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace Contracts.DataServices {
+	
+	/// <summary>
+	/// Интейрфейс репозитория БД.
+	/// </summary>
+	/// <typeparam name="T">Модель</typeparam>
+	public interface IRepositoryBase<T> {
+
+		public IQueryable<T> FindAll(bool trackChanges);
+		public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+		void Create(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+	}
+}
---
+++ HeadHunterAnalyzer.API/Repository/RepositoryManager.cs
@@ -0,0 +1,55 @@
+﻿using Contracts.DataServices;
+using Entities;
+
+namespace Repository {
+	
+	/// <summary>
+	/// Класс содержащий ссылки на репозитории моделей
+	/// </summary>
+	public class RepositoryManager : IRepositoryManager {
+
+		private readonly RepositoryContext _repositoryContext;
+		private ICompanyRepository _companyRepository;
+		private IVacancyRepository _vacancyRepository;
+		private IWordRepository _wordRepository;
+
+		public RepositoryManager(RepositoryContext repositoryContext) {
+
+			_repositoryContext = repositoryContext;
+		}
+
+		public ICompanyRepository Companies { 
+		
+			get {
+
+				if (_companyRepository == null)
+					_companyRepository = new CompanyRepository(_repositoryContext);
+
+				return _companyRepository;
+			}
+		}
+
+		public IVacancyRepository Vacancies { 
+			get {
+
+				if (_vacancyRepository == null)
+					_vacancyRepository = new VacancyRepository(_repositoryContext);
+
+				return _vacancyRepository;
+			} 
+		}
+
+		public IWordRepository Words { 
+			
+			get {
+
+				if (_wordRepository == null)
+					_wordRepository = new WordRepository(_repositoryContext);
+
+				return _wordRepository;
+			}
+		}
+
+		public async Task SaveAsync() => await _repositoryContext.SaveChangesAsync();
+	}
+}
